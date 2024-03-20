0a73bd26bd4dfb80dc15b7436a67b14806968db4
Ricardo Afonso <r.afonsomontero@gmail.com>
2024-03-20 02:28:14 -0300

    Added functionalities

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const mockUpStrand = () => {
 };
 
 function pAequorFactory(number, dnaArray) {
-  const pAequor = {
+  let pAequor = {
     'specimenNum': number,
     'dna': dnaArray,
     mutate() {
@@ -26,13 +26,44 @@ function pAequorFactory(number, dnaArray) {
         dnaToBeMutated = returnRandBase();
       }
       this.dna[indexToBeChanged] = dnaToBeMutated;
+    },
+    compareDNA(pAequorObject) {
+      dnaToCompareWith = pAequorObject.dna;
+      let count = 0;
+
+      for (let i = 0; i <= this.dna.length; i++) {
+        if (this.dna[i] === dnaToCompareWith[i]) {
+          count++;
+        };
+      };
+
+      let percentageOfCommon = Math.floor((count / this.dna.length) * 100);
+      console.log(`specimen #1 and specimen #2 have ${percentageOfCommon}% DNA in common`);
+    },
+    willLikelySurvive() {
+      let count = 0;
+      
+      for (let base of this.dna) {
+        if (base === 'C' || base === 'G') {
+          count++;
+        };
+      };
+      return Math.floor((count / this.dna.length) * 100) >= 60;
     }
   };
 
   return pAequor;
 };
 
-let pAequor1 = pAequorFactory(1, mockUpStrand());
-console.log(pAequor1.dna);
-pAequor1.mutate();
-console.log(pAequor1.dna);
+let samples = [];
+
+while (samples.length < 30) {
+  let sampleNumber = samples.length + 1;
+  let pAequorSample = pAequorFactory(sampleNumber, mockUpStrand());
+
+  if (pAequorSample.willLikelySurvive()) {
+    samples.push(pAequorSample);
+  };
+};
+
+console.log(samples);
